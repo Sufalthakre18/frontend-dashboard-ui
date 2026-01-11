936afcfef62c45442797e4a0988b17017dcef05a
Sufal Thakre <sufalthakre1@gmail.com>
2026-01-11 09:32:39 +0530

    Initial project setup with React and Tailwind with vite

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # frontend-dashboard-ui
 Pixel-perfect dashboard UI built using React and Tailwind CSS as part of a frontend assignment.
+
---
+++ dashboard-ui/src/App.jsx
@@ -0,0 +1,10 @@
+
+function App() {
+  return (
+    <>
+    <h1 className='italic text-6xl bg-red-700'>Begiining of project</h1>
+    </>
+  )
+}
+
+export default App
